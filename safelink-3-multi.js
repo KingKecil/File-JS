45f045380ea26ee2cd7dfba73907753370e54345
KingKecil <adnanyananda@gmail.com>
2016-12-05 23:13:28 +0700

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="2016/11/cerita-cinta-sedih-romantis-yang.html"}
 if(x=="2"){xxx="2016/11/cerita-cinta-kebahagiaan-cinta-by.html"}
-if(x=="3"){xxx="2016/11/lagu-terakhir-untuk-ita.htmll"}
+if(x=="3"){xxx="2016/11/lagu-terakhir-untuk-ita.html"}
 if(x=="4"){xxx="2016/11/senyum-terakhir-kisah-cinta-menyedihkan.html"}
 if(x=="5"){xxx="2016/11/cerita-cinta-romantis-2016-cerpen.html"}a_to_ve[i].href="https://buattestingajaini.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
